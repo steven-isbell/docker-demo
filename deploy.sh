34e62a21b131f0c2cd3118bd4cafd13664d8edde
Steven Isbell <steveni_92@msn.com>
2018-07-05 23:24:28 -0500

    docker

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd ~/DevMountain/lessons/mentor-brownbags/docker-demo
 git pull
 docker build -f Dockerfile-prod -t docker-demo-image .
-docker rm -f docer-demo-container
+docker rm -f docker-demo-container
 docker run -d -p 3001:3001 --restart always --name="docker-demo-container" -i docker-demo-image
 docker container prune -f
 docker image prune -f
